b8507c781d2a7e6019caa3dcd45c296ce79a3982
Apexforge549 <apexforgeesports@gmail.com>
2026-03-04 20:36:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,16 +2,14 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+print("TOKEN:", os.getenv("BOT_TOKEN"))
 
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN is missing! Add it in Railway Variables.")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Wow! The bot is connected 🚀")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()

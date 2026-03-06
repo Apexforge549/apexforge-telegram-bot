b576e6eebf9e285ac65017336e9a0fdc4288330f
Apexforge549 <apexforgeesports@gmail.com>
2026-03-06 22:46:49 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,20 @@
+import os
+from pymongo import MongoClient
 
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = None
+
+# MongoDB connection
+try:
+    client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
+    client.server_info()
+    print("MongoDB connected successfully")
+except Exception as e:
+    print("MongoDB connection error:", e)
+
+if client:
+    db = client["esports_db"]
+    users_collection = db["users"]
+else:
+    users_collection = None

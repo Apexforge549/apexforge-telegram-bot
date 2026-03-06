3a1c06f6db57fb649558041e2fb929d07bbd8255
Apexforge549 <apexforgeesports@gmail.com>
2026-03-06 22:48:49 +0530

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -1,1 +1,12 @@
-keyboar
+from telegram import ReplyKeyboardMarkup
+
+# Main keyboard after registration
+main_keyboard = ReplyKeyboardMarkup(
+    [
+        ["🏆 Tournaments"],
+        ["📅 Check-in", "👤 Profile"],
+        ["💰 Balance", "ℹ️ About"],
+        ["💸 Withdraw", "💳 Deposit"]
+    ],
+    resize_keyboard=True
+)

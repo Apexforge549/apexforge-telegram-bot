b6665db567146d563356e0c88b7cb6dc94c87896
Apexforge549 <apexforgeesports@gmail.com>
2026-04-02 10:33:14 +0530

    Update withdraw.py

--- handlers/withdraw.py
+++ handlers/withdraw.py
@@ -1,1 +1,12 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+from keyboards import withdraw_keyboard
 
+# Shows the Withdraw menu
+async def withdraw(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    await update.message.reply_text(
+        "📤 *Withdraw Menu*\n\nSelect an option below:",
+        parse_mode="Markdown",
+        reply_markup=withdraw_keyboard
+    )

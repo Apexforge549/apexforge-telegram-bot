911c9999ac34bba2c63962e2aaef5c2264834629
Apexforge549 <apexforgeesports@gmail.com>
2026-04-03 18:57:01 +0530

    Update tournaments.py

--- handlers/tournaments.py
+++ handlers/tournaments.py
@@ -1,1 +1,12 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+from keyboards import tournament_keyboard
 
+# Opening the Tournaments keyboard
+async def tournaments(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    await update.message.reply_text(
+        "🏆 *Tournaments Menu*\n\nSelect an option:",
+        parse_mode="Markdown",
+        reply_markup=tournament_keyboard
+    )

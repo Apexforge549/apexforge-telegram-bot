d1827710ebdde05dacbb3462e56d2d18a4e70b15
Apexforge549 <apexforgeesports@gmail.com>
2026-04-03 20:32:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -146,7 +146,7 @@ def main():
     # Conversation handler for game profile button
     game_profile_conv = ConversationHandler(
     entry_points=[
-        MessageHandler(filters.Regex("^📊 Game Profile$"), game_profile)
+        MessageHandler(filters.Regex("^📊 Game Profile$"), game_profile),
         CallbackQueryHandler(change_profile_start, pattern="^change_profile$")
     ],
     states={

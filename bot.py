2ed2b52d5a24a39cad226408c4bfd4500aca8055
Apexforge549 <apexforgeesports@gmail.com>
2026-04-05 16:54:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -177,8 +177,25 @@ def main():
 
     app.add_handler(game_profile_conv)
 
+    # Conversation handler for update upi id in profile.py
+    # Delete this later when withdraw.py activates
+    upi_conv = ConversationHandler(
+        entry_points=[
+            MessageHandler(filters.Regex("^👤 Profile$"), game_profile),
+            CallbackQueryHandler(update_upi_start, pattern="^update_upi$")
+        ],
+        states={
+            UPI_INPUT: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, handle_upi_input)
+            ]
+        },
+        fallbacks=[]
+    )
+
+    app.add_handler(upi_conv)
+
     #handler for profile button
-    app.add_handler(MessageHandler(filters.Regex("^👤 Profile$"), profile))
+    #app.add_handler(MessageHandler(filters.Regex("^👤 Profile$"), profile))
 
     #handler for balance button
     app.add_handler(MessageHandler(filters.Regex("^📊 Balance$"), balance))
@@ -216,22 +233,6 @@ def main():
 
     # Handler for about button
     app.add_handler(MessageHandler(filters.Regex("^ℹ️ About$"), about))
-
-    # Conversation handler for update upi id in profile.py
-    # Delete this later when withdraw.py activates
-    upi_conv = ConversationHandler(
-        entry_points=[
-            CallbackQueryHandler(update_upi_start, pattern="^update_upi$")
-        ],
-        states={
-            UPI_INPUT: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, handle_upi_input)
-            ]
-        },
-        fallbacks=[]
-    )
-
-    app.add_handler(upi_conv)
 
     print("Bot is running...")
     app.run_polling(drop_pending_updates=True)

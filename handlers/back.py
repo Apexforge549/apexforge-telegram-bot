3585e59ce923cf9c41c5569dd7ad9ec6716936f8
Apexforge549 <apexforgeesports@gmail.com>
2026-04-09 06:12:12 +0530

    Update back.py

--- handlers/back.py
+++ handlers/back.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import ContextTypes
-from keyboards import main_keyboard
+from keyboards import main_keyboard, admin_keyboard
+from utils.admin_check import is_admin
 
 
 async def go_back(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -8,4 +9,13 @@ async def go_back(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "🔙 Back to main menu",
         reply_markup=main_keyboard
+    )
+
+async def admin_go_back(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not is_admin(update.effective_user.id):
+        return
+
+    await update.message.reply_text(
+        "🔙 Back to main menu",
+        reply_markup=admin_keyboard
     )
